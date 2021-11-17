ba978b080cae082710f1f6547ed2d1a915eb065a
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2021-11-17 22:35:45 +0530

    haha

--- src/com/saksham/Student.java
+++ src/com/saksham/Student.java
@@ -1,5 +1,13 @@
 package com.saksham;
 
+import java.util.List;
+import java.util.TreeMap;
+
 public class Student {
+    List<Course> courses;
+    String name;
+    int id;
+    int daysAttended;
+    TreeMap<String,Integer> marksList;
 
 }
